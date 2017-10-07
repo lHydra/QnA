7551be65152f47a76c02ea71b295ba85460ba597
George <skywalqqer@yandex.ru>
2017-10-07 23:07:13 +0300

    Attachment model

---
+++ spec/acceptance/question/attach_files_spec.rb
@@ -0,0 +1,23 @@
+require_relative '../acceptance_helper'
+
+feature 'Attach files', %q{
+  In order to show details of the problem
+  As an User
+  I want to be able to attach files
+} do
+
+  given(:user) { create(:user) }
+
+  background do
+    sign_in(user)
+    visit new_question_path
+  end
+
+  scenario 'User adds the file to the question' do
+    fill_in 'Title', with: 'Some title'
+    fill_in 'Body', with: 'Some body'
+    attach_file 'File', "#{Rails.root}/spec/spec_helper.rb"
+
+    click_on 'Create'
+  end
+end
